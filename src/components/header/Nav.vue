759e4f1422a7fa92b7cd050d3b979ec110f916a3
lorenzo91347 <lorenzo.losito91@gmail.com>
2024-01-07 15:54:14 +0100

    feat:dropdown menu on Nav changed to click

--- src/components/header/Nav.vue
+++ src/components/header/Nav.vue
@@ -1,77 +1,63 @@
 <script>
-export default{
-    data(){
-        return{
-            listShown: false,
-            listHidden: false
-        }
-    },
-    methods:{
-        showList(){
-            this.listShown = true,
-            this.listHidden = false
-        },
-        hidList(){
-            this.listShown = false,
-            this.listHidden = true
-        }
-    },
-    //props for HeaderMain component
-    props:{
-        value : "number"
-    },
-   
-}
+export default {
+	data() {
+		return {
+			listShown: false,
+        
+		};
+	},
+	
+		
+	
+	//props for HeaderMain component
+	props: {
+		value: "number",
+	},
+};
 </script>
 <template>
-    <!--When rendered inside HeaderMain the prop will be cycled -->
-        <div @mouseover="showList()">
-            <li>{{ value }}</li>
-            <ul v-if="listShown"  @mouseover="showList()" @mouseleave="hidList()" :class="{ active: listShown }">
-                <li class="dropdown">Item 1</li>
-                <li class="dropdown">Item 1</li>
-                <li class="dropdown">Item 1</li>                
-            </ul>
-        </div>
-    
+	<!--When rendered inside HeaderMain the prop will be cycled -->
+	<div>
+		<li @click="listShown = !listShown " >{{ value }}</li>
+		<ul v-if="listShown"  :class="{ active: listShown }">
+			<li class="dropdown">Item 1</li>
+			<li class="dropdown">Item 1</li>
+			<li class="dropdown">Item 1</li>
+		</ul>
+        <div v-else></div>
+	</div>
 </template>
 <style scoped lang="scss">
-@use '../../assets/styles/colors.scss' as *;
-div{
-display: flex;
-flex-direction: column;
-align-items: center;
-position:relative;
+@use "../../assets/styles/colors.scss" as *;
+div {
+	position: relative;
 
-li{
-    text-align: left;
-    width: 80px;
-    &:hover{
-color:$color-lima-light;
-}}
-ul{
-    list-style: none;
-    padding-left: 0;
-    border: 1px solid $color-lima-light;
-    background-color: rgb(197, 197, 197);
- .dropdown{
-  width: 80px;
- }
-.dropdown:nth-child(-n + 2){
-    border-bottom: 1px solid $color-lima-light;
-
+	li {
+		text-align: left;
+		cursor: pointer;
+		width: 80px;
+		&:hover {
+			color: $color-lima-light;
+		}
+	}
+	ul {
+		list-style: none;
+		cursor: pointer;
+		padding-left: 0;
+		border: 1px solid $color-lima-light;
+		background-color: rgb(197, 197, 197);
+		.dropdown {
+			width: 80px;
+		}
+		.dropdown:nth-child(-n + 2) {
+			border-bottom: 1px solid $color-lima-light;
+		}
+		.active {
+			position: absolute;
+			top: 20px;
+		}
+	}
 }
-.active{
-    z-index: 4;
-    position: absolute;
-    top: 20px;
-    
-}
-}
-}
-
-
-
-
 </style>
+
 
